04ec752ed50a70464aed263130a8332a11e6fbce
Ryan Oates <ryanoatsie@outlook.com>
2024-11-26 18:32:58 -0800

    add adjacency list representation for the graph; update comments and remove image reference

--- quiz2/src/Adjlistandmatrix.java
+++ quiz2/src/Adjlistandmatrix.java
@@ -1,6 +1,18 @@
 // Show the adjacency list and adjacency representation of this graph. You can draw on paper and upload screenshot.
 
-quiz2/src/2DC98639-5D6E-4AE5-A885-5ABB9E391073.jpeg
+//quiz2/src/2DC98639-5D6E-4AE5-A885-5ABB9E391073.jpeg
+
+Adjacency List:
+1 -> 2, 3, 4
+2 -> 1, 3
+3 -> 1, 2, 5, 6
+4 -> 1
+5 -> 3 , 6 
+6 -> 5
+7 -> 0
+
+
+
 
 //For the graph shown, implement it in Java, you can choose any of the graph representation techniques.
 
