97ada273bfc47e921643df5b273eac16322ed37f
Ryan Oates <ryanoatsie@outlook.com>
2024-11-26 18:37:36 -0800

    update adjacency list format and add adjacency matrix representation for the graph

--- quiz2/src/Adjlistandmatrix.java
+++ quiz2/src/Adjlistandmatrix.java
@@ -1,17 +1,26 @@
 // Show the adjacency list and adjacency representation of this graph. You can draw on paper and upload screenshot.
 
 //quiz2/src/2DC98639-5D6E-4AE5-A885-5ABB9E391073.jpeg
-
+/* 
 Adjacency List:
-1 -> 2, 3, 4
-2 -> 1, 3
-3 -> 1, 2, 5, 6
-4 -> 1
-5 -> 3 , 6 
-6 -> 5
-7 -> 0
+1 | 2, 3, 4
+2 | 1, 3
+3 | 1, 2, 5, 6
+4 | 1
+5 | 3 , 6 
+6 | 5
+7 | 0
 
+Adjacency Matrix:
 
+   1 2 3 4 5 6 7
+1  0 1 1 1 0 0 0
+2  1 0 1 0 0 0 0
+3  1 1 0 0 1 1 0
+4  1 0 0 0 0 0 0
+5  0 0 1 0 0 1 0
+6  0 0 0 0 1 0 0
+7  0 0 0 0 0 0 0 */
 
 
 //For the graph shown, implement it in Java, you can choose any of the graph representation techniques.
