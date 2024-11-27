1734821b182c6c4b132f678c3ef9e5d93e2679b5
Ryan Oates <ryanoatsie@outlook.com>
2024-11-26 19:24:59 -0800

    remove unused GraphEntitiesOfGraph class and related methods; simplify main method in Adjlistandmatrix class

--- quiz2/src/Adjlistandmatrix.java
+++ quiz2/src/Adjlistandmatrix.java
@@ -24,57 +24,14 @@ Adjacency Matrix:
 
 
 //For the graph shown, implement it in Java, you can choose any of the graph representation techniques.
+public class Adjlistandmatrix {
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class GraphEntitiesOfGraph<T, V> {
-    private HashMap<T, List<V>> adjVertices; 
-
- public HashMap<T, List<V>> CreateAdjVerticesHashMap(HashMap<T, List<V>> adjVertices) {
-    this.adjVertices = adjVertices;
-    return adjVertices;
- }
-
- public HashMap<T, List<V>> getAdjVerticesMap() {
-    return adjVertices;
- }
-
-    public void placeSourceAndDestination(T source, V destination) {
-        adjVertices.putIfAbsent(source, new ArrayList<>());
-        adjVertices.get(source).add(destination);
-    }
-    public void printGraph() {
-        for (Map.Entry<T, List<V>> entry : adjVertices.entrySet()) {
-            System.out.println("Source Node: " + entry.getKey() + ": " + "Destination Node: " + entry.getValue());
-        }
-    }
     public static void main(String[] args) {
         GraphEntitiesOfGraph<Integer, Integer> graphOne = new GraphEntitiesOfGraph<>();
         graphOne.CreateAdjVerticesHashMap(new HashMap<Integer, List<Integer>>());
-        graphOne.placeSourceAndDestination(1, 2);
-        graphOne.placeSourceAndDestination(1, 3);
-        graphOne.placeSourceAndDestination(1, 4);
-        graphOne.placeSourceAndDestination(2, 1);
-        graphOne.placeSourceAndDestination(2, 3);   
-        graphOne.placeSourceAndDestination(3, 1);
-        graphOne.placeSourceAndDestination(3, 2);
-        graphOne.placeSourceAndDestination(3, 5);
-        graphOne.placeSourceAndDestination(3, 6);
-        graphOne.placeSourceAndDestination(4, 1);
-        graphOne.placeSourceAndDestination(5, 3);
-        graphOne.placeSourceAndDestination(5, 6);
-        graphOne.placeSourceAndDestination(6, 5);
-        graphOne.placeSourceAndDestination(7, 0);
-        graphOne.getAdjVerticesMap();
-
-        graphOne.printGraph();
     }
 }
-
-/* Output : 
+    /* Output : 
 Source Node: 1: Destination Node: [2, 3, 4]
 Source Node: 2: Destination Node: [1, 3]
 Source Node: 3: Destination Node: [1, 2, 5, 6]
